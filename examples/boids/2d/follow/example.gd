7bc004cc1251f0809d97ffa2f2c9f8ff175dfbeb
dusk <y.bera003.06@protonmail.com>
2024-08-30 01:32:01 +0300

    wip

--- examples/boids/2d/follow/example.gd
+++ examples/boids/2d/follow/example.gd
@@ -1,14 +1,19 @@
 extends Node2D
 
 func _ready() -> void:
-	for i in 40: spawnBoid()
+	for flock in get_children():
+		if flock is not Flock2D: continue
+		var color = Color(randf_range(0.8, 1.5), randf_range(0.8, 1.5), randf_range(0.2, 1.5), 1)
+		for i in 100: spawnBoid(flock, color)
 
 func _process(delta: float) -> void:
-	$Path2D/PathFollow2D.progress_ratio += delta * 0.1
+	$Path2D/PathFollow2D.progress_ratio += delta * 0.125
+	$Path2D2/PathFollow2D.progress_ratio += delta * 0.1
+	$Path2D3/PathFollow2D.progress_ratio += delta * 0.08
 
-func spawnBoid() -> void:
+func spawnBoid(flock: Flock2D, color: Color) -> void:
 	var boid: Boid2D = preload("../example_boid.tscn").instantiate()
 	var screensize := get_viewport_rect().size
-	boid.modulate = Color(randf(), randf(), randf(), 1)
+	boid.modulate = color
 	boid.global_position = Vector2((randf_range(200, screensize.x - 200)), (randf_range(200, screensize.y - 200)))
-	$Flock.add_child(boid)
+	flock.add_child(boid)
--- examples/boids/2d/simple/example.gd
+++ examples/boids/2d/simple/example.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 func _ready() -> void:
 	for flock in get_children():
-		for i in 1000: spawnBoid(flock)
+		if flock is not Flock2D: continue
+		var color = Color(randf(), randf(), randf(), 1)
+		for i in 2000: spawnBoid(flock, color)
 
-func spawnBoid(flock: Flock2D) -> void:
+func spawnBoid(flock: Flock2D, color: Color) -> void:
 	var boid: Boid2D = preload("../example_boid.tscn").instantiate()
 	var screensize := get_viewport_rect().size
-	boid.modulate = Color(randf(), randf(), randf(), 1)
+	boid.modulate = color
 	boid.global_position = Vector2((randf_range(200, screensize.x - 200)), (randf_range(200, screensize.y - 200)))
 	flock.add_child(boid)
