346cde53d23b16850419cb38ad15471259e62f08
dusk <y.bera003.06@protonmail.com>
2024-09-24 17:01:41 +0300

    build: zip the addon in script

--- rust/package-release.sh
+++ rust/package-release.sh
@@ -7,3 +7,5 @@ cross +nightly build -Zbuild-std --release --target wasm32-unknown-emscripten
 cp -f target/x86_64-unknown-linux-gnu/release/libboids.so ../addons/boids/lib/boids.x86.so
 cp -f target/x86_64-pc-windows-gnu/release/boids.dll ../addons/boids/lib/boids.x86.dll
 cp -f target/wasm32-unknown-emscripten/release/boids.wasm ../addons/boids/lib/boids.wasm
+
+zip -r ../../boids-release.zip ../addons ../examples ../README.md ../LICENSE.txt
