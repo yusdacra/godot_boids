ce8e663c14c23ebbd3c61334d2899e5a30e2f2a2
dusk <y.bera003.06@protonmail.com>
2024-09-24 17:19:05 +0300

    build: fix zipped paths

--- rust/package-release.sh
+++ rust/package-release.sh
@@ -4,8 +4,10 @@ cross build --release --target x86_64-unknown-linux-gnu
 cross build --release --target x86_64-pc-windows-gnu
 cross +nightly build -Zbuild-std --release --target wasm32-unknown-emscripten
 
-cp -f target/x86_64-unknown-linux-gnu/release/libboids.so ../addons/boids/lib/boids.x86.so
-cp -f target/x86_64-pc-windows-gnu/release/boids.dll ../addons/boids/lib/boids.x86.dll
-cp -f target/wasm32-unknown-emscripten/release/boids.wasm ../addons/boids/lib/boids.wasm
+cd ..
 
-zip -r ../../boids-release.zip ../addons ../examples ../README.md ../LICENSE.txt
+cp -f rust/target/x86_64-unknown-linux-gnu/release/libboids.so addons/boids/lib/boids.x86.so
+cp -f rust/target/x86_64-pc-windows-gnu/release/boids.dll addons/boids/lib/boids.x86.dll
+cp -f rust/target/wasm32-unknown-emscripten/release/boids.wasm addons/boids/lib/boids.wasm
+
+zip -r ../boids-release.zip addons examples README.md LICENSE.txt
